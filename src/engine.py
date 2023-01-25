eee7c083ad59bb48d32ee0da98658afd5b882f21
Enrico Sanna <antani@tiscali.it>
2023-01-25 18:01:37 +0100

    adding player management in engine class

--- src/engine.py
+++ src/engine.py
@@ -15,6 +15,7 @@ from pygame.constants import (
     USEREVENT,
 )
 
+from player import Player
 from utils import get_sound, get_sprite, print_text
 
 
@@ -30,18 +31,21 @@ class Engine:
     def __init__(self, title):
         pygame.init()
         self._canvas = display.set_mode((800, 600))
-        self._bg_image = get_sprite("bg_grass.jpg", (800, 600), False)
-        self._bg_sound = get_sound("background.wav")
+        self._bg = {
+            "image": get_sprite("bg_grass.jpg", (800, 600), False),
+            "sound": get_sound("background.wav"),
+        }
         self._clock = pygame.time.Clock()
         self._state = self.GAME_STATE["START"]
         pygame.display.set_caption(title)
 
         self.direction_key = self.STARTING_DIRECTION
         self.is_highscore = False
+        self.player = Player()
 
     def mainloop(self):
         """Min loop"""
-        self._bg_sound.play(-1)
+        self._bg["sound"].play(-1)
 
         while True:
             self.input()
@@ -51,6 +55,7 @@ class Engine:
     def _init(self):
         self.direction_key = self.STARTING_DIRECTION
         self.is_highscore = False
+        self.player.init()
 
     def input(self):
         """User input management"""
@@ -75,19 +80,25 @@ class Engine:
     def update(self):
         """Game data update"""
         if self._state == self.GAME_STATE["RUN"]:
-            pass
+            self.player.move(self.direction_key)
+
+            if not self.player.is_alive():
+                pygame.time.set_timer(self.HIGHSCORE_EVENT, 3000, 1)
+                self._state = self.GAME_STATE["END"]
 
     def draw(self):
         """Game items draw"""
-        self._canvas.blit(self._bg_image, (0, 0))
+        self._canvas.blit(self._bg["image"], (0, 0))
 
         if self._state == self.GAME_STATE["START"]:
             message = {"SNAKE": 70, "Press ENTER to start": 50}
             print_text(self._canvas, message)
         else:
             if self._state == self.GAME_STATE["END"]:
-                message = {"You died!": 70, f"Your score is: {self.score.points}": 50}
+                message = {"You died!": 70, "Your score is: 0": 50}
                 print_text(self._canvas, message)
+            else:
+                self.player.draw(self._canvas)
 
         display.flip()
         self._clock.tick(4)
