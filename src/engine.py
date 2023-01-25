e5b02e09cefad90e4d608ef83a34ec7a2468b12f
Enrico Sanna <antani@tiscali.it>
2023-01-25 15:28:06 +0100

    adding directories tree skeleton, main and engine class

---
+++ src/engine.py
@@ -0,0 +1,84 @@
+""" Module docstring """
+
+import sys
+import pygame
+from pygame import display
+from pygame.constants import (
+    K_UP,
+    K_DOWN,
+    K_LEFT,
+    K_RIGHT,
+    K_ESCAPE,
+    K_RETURN,
+    KEYDOWN,
+    QUIT,
+    USEREVENT
+)
+
+class Engine:
+    """ Class docstring """
+    GAME_STATE = {
+        'START': 1,
+        'RUN': 2,
+        'END': 3,
+        'HIGHSCORE': 4
+    }
+
+    HIGHSCORE_EVENT = USEREVENT + 1
+
+    STARTING_DIRECTION = K_RIGHT
+
+    def __init__(self, title):
+        pygame.init()
+        self._canvas = display.set_mode((800, 600))
+        self._clock = pygame.time.Clock()
+        self._state = self.GAME_STATE['START']
+        pygame.display.set_caption(title)
+
+        self.direction_key = self.STARTING_DIRECTION
+        self.is_highscore = False
+
+    def mainloop(self):
+        """ Min loop """
+        while True:
+            self.input()
+            self.update()
+            self.draw()
+
+    def _init(self):
+        self.direction_key = self.STARTING_DIRECTION
+        self.is_highscore = False
+
+    def input(self):
+        """ User input management """
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                sys.exit()
+            elif event.type == self.HIGHSCORE_EVENT:
+                self._state = self.GAME_STATE['HIGHSCORE']
+
+            if event.type == KEYDOWN:
+                if event.key in [K_UP, K_DOWN, K_LEFT, K_RIGHT]:
+                    self.direction_key = event.key
+                elif event.key == K_ESCAPE:
+                    sys.exit()
+                elif (
+                    event.key == K_RETURN
+                    and self._state in [
+                        self.GAME_STATE['START'],
+                        self.GAME_STATE['HIGHSCORE']]
+                ):
+                    self._init()
+                    self._state = self.GAME_STATE['RUN']
+
+    def update(self):
+        """ Game data update """
+        if self._state == self.GAME_STATE['RUN']:
+            pass
+
+    def draw(self):
+        """ Game items draw """
+        self._canvas.fill((0, 0, 0))
+
+        display.flip()
+        self._clock.tick(4)
---
+++ src/main.py
@@ -0,0 +1,13 @@
+"""
+Snake - python
+"""
+
+from engine import Engine
+
+def main():
+    """ Main function """
+    game = Engine('Snake')
+    game.mainloop()
+
+if __name__ == '__main__':
+    main()
